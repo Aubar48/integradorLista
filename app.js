cfb166105a3f6bf86f0714ecb644a99f3d377af3
“Nahuel <nnahuell.gb@hotmail.com>
2023-05-19 12:12:58 -0300

    manejo de errores

--- app.js
+++ app.js
@@ -7,46 +7,47 @@ let respuesta = ""
 //argv
 const button = argv[2].toLowerCase()
 //msj antes del if
-console.log('Bienvenidos ala appTarea => Comandos:'.blue);
-console.log('listar: trae la lista de tareas de la base de datos'.blue);
-console.log('buscar: trae la lista de tareas de la base de datos por estado'.blue);
-console.log('eliminar: elimina por id, una tarea, de la base de datos '.blue);
-console.log('editar: recibe un id y edita titulo y estado de una tarea, de la base de datos'.blue);
 // interfaz if con switch
-if (typeof button === 'string') {
-  switch (button) {
-    case 'listar':
-      app.listarTareas()
-      break;
-    case 'agregar':
-      let id = +argv[3]
-      let titulo = argv[4]
-      if ([id,titulo].includes(undefined)) {     
-        return console.log('ERROR: Faltan datos : id, titulo, estado de la nueva tarea'.red)}
-      respuesta = app.agregarTarea(id, titulo)
-      break;
-    case 'buscar':
-      let filtro = argv[3]
-      if (filtro === undefined) {
-        return console.log('ERROR: Faltan datos'.red)}
-        respuesta = app.buscarTarea(filtro)
+  app.textMenu()
+  if (typeof button === 'string') {
+    switch (button) {
+      case 'listar':
+        app.listarTareas()
         break;
-    case 'eliminar':
-     let i = +argv[3]
-      respuesta = app.eliminarTarea(i)
-      break;
-      case 'editar':
-      let id2 = +argv[3]
-      let titulo2 = argv[4]
-      let estado2 = argv[5]
-      if ([id2,titulo2, estado2].includes(undefined)) {     
-        return console.log('ERROR: Faltan datos : id, titulo, estado de la tarea a editar'.red)}
-      respuesta = app.modificarTarea(id2, titulo2, estado2)
-      break;
-    default:
-      console.log('ERROR: debes ingresar el comando "listar" "buscar" "eliminar" "editar" para ejecutar esta acción.'.red);
-      break;
+      case 'agregar':
+        let id = +argv[3]
+        let titulo = argv[4]
+        if ([id,titulo].includes(undefined)) {     
+          return console.log(`ERROR: Faltan datos : id, titulo, estado de la nueva tarea`.bold.red)}
+        respuesta = app.agregarTarea(id, titulo)
+        break;
+      case 'buscar':
+        let filtro = argv[3].toLocaleLowerCase()
+        if (filtro.includes(undefined || null || NaN)) {
+          return console.log(`ERROR: Los estados son : terminada, pediente, no iniciada`.bold.red)}
+          respuesta = app.buscarTarea(filtro)
+          break;
+      case 'eliminar':
+       let i = +argv[3]
+        respuesta = app.eliminarTarea(i)
+        break;
+        case 'editar':
+        let id2 = +argv[3]
+        let titulo2 = argv[4]
+        let estado2 = argv[5]
+        if ([id2,titulo2, estado2].includes(undefined)) {     
+          return console.log(`ERROR: Faltan datos : id, titulo, estado, de la tarea a editar`.bold.red)}
+        respuesta = app.modificarTarea(id2, titulo2, estado2)
+        break;
+        case 'salir':
+          app.salir()
+          break;
+      default:
+        console.log(`ERROR: debes ingresar el comando "listar" "agregar" "buscar" "eliminar" "editar" "salir" para ejecutar esta acción.`.bold.red);
+        break;
+    }
+  } else {
+    console.log(`ERROR: debes ingresar una cadena de texto para ejecutar la acción, checkea el menu.`.bold.red);
   }
-} else {
-  console.log('ERROR: debes ingresar una cadena de texto para ejecutar la acción, checkea el menu.'.red);
-}
+
+
--- appTarea/funcionesDeTareas.js
+++ appTarea/funcionesDeTareas.js
@@ -7,21 +7,21 @@ const { leerJSON, escribirJSON } = require('./leerEscribir')
 //funcionalidad
 module.exports = {
     listarTareas: () => {
-        console.log("Listado de tareas: ");
+        console.log(`Listado de tareas: `.bold.yellow);
         dataTarea.forEach((tarea) => {
-            console.log(`${tarea.id}-> ${tarea.titulo} --> ${tarea.estado}`.green);
+            console.log(`${tarea.id}-> ${tarea.titulo} --> ${tarea.estado}`.bold.green);
         });
     },
     agregarTarea: (id, titulo) => {
         const tareaExistente = dataTarea.find(tarea => tarea.id === id);
         if (tareaExistente) {
-            console.log(`ERROR: El ID ${id} ya existe en la base de datos.`.red);
+            console.log(`ERROR: El ID ${id} ya existe en la base de datos.`.bold.red);
             return;
         } else {
             let nuevaTarea = new Tarea(id, titulo);
             dataTarea.push(nuevaTarea);
             escribirJSON(dataTarea);
-            console.log(`El nuevo ID ${id} se agregó a la lista de tareas de la base de datos con éxito.`);
+            console.log(`El nuevo ID ${id} se agregó a la lista de tareas de la base de datos con éxito.`.bold.green);
             return nuevaTarea;
         }
 
@@ -37,27 +37,45 @@ module.exports = {
         });
     },
     buscarTarea: function (estado) {
-        console.log("Listado de tareas " + estado + ": ");
+        if (!(estado == `pendiente` || estado == `no iniciada` || estado == `terminada`)){
+            return console.log(`ERROR: Los estados son : terminada, pediente, no iniciada`.bold.red)
+        } else {
+        console.log(`Listado de tareas  ${estado}: `.bold.yellow);
         dataTarea.forEach((tarea) => {
-            if (tarea.estado === estado && estado === "Terminada") {
+            if (tarea.estado.toLowerCase() === estado.toLowerCase() && estado == "terminada") {
                 console.log(`${tarea.id} -> Titulo: ${tarea.titulo} --> Estado: ${tarea.estado}`.green);
-            } else if (tarea.estado === estado && estado === "Pendiente") {
+            } else if (tarea.estado.toLowerCase() === estado.toLowerCase() && estado == "no iniciada") {
                 console.log(`${tarea.id} -> Titulo: ${tarea.titulo} --> Estado: ${tarea.estado}`.red);
-            } else if (tarea.estado === estado && estado === "No iniciada") {
+            } else if (tarea.estado.toLowerCase() === estado.toLowerCase() && estado == "pendiente" ) {
                 console.log(`${tarea.id} -> Titulo: ${tarea.titulo} --> Estado: ${tarea.estado}`.yellow);
             }
         })
+    }
     },
     modificarTarea: (id, titulo, estado) => {
         dataTarea.forEach((tarea) => {
-            if (tarea.id + 1 == id) {
+            if (tarea.id  == id) {
                 tarea.titulo = titulo;
                 tarea.estado = estado;
                 escribirJSON(dataTarea)
-                console.log('Tarea modificado con exito'.green);
+                console.log(`El id: ${tarea.id}, de la base de datos de tarea, se a modificado con exito`.bold.green);
                 return tarea;
+            }else {
+                return console.log(`ERROR: El id ${id} no existe en la base de datos.`.bold.red);
+                
             }
         });
+    },
+    textMenu: () => {
+        console.log(`1 -> Bienvenidos ala appTarea => Comandos:`.bold);
+        console.log(`2 -> listar: trae la lista de tareas de la base de datos`.blue);
+        console.log(`3 -> buscar: trae la lista de tareas de la base de datos por estado`.blue);
+        console.log(`4 -> eliminar: elimina por id, una tarea, de la base de datos`.blue);
+        console.log(`5 -> editar: recibe un id y edita titulo y estado de una tarea, de la base de datos`.blue);
+        console.log(`6 -> salir: Para finalizar el programa`.blue);
+    },
+    salir : () => {
+        console.log(`Hasta luego :)`.bold.green);
     }
 }
 
